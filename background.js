9e973c1f7a09298081190d71ff17439d33cb4836
Ritik <saxenar444@gmail.com>
2017-01-31 20:23:27 +0530

    completely functional-finished

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+var clicked = Number(0);
 chrome.browserAction.onClicked.addListener(function(tab){
-  chrome.tabs.sendMessage(tab.id,{clicked:true});
+  clicked = 1-clicked;
+  console.log(clicked+"sd");
+  chrome.tabs.sendMessage(tab.id,{clicked:clicked});
 });
--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+var color = [];
 chrome.runtime.onMessage.addListener(function(msg,sender){
   console.log("dsd");
-  if(msg.clicked){
-    console.log("aasd");
-    var a = document.getElementsByTagName("a");
+  var a = document.getElementsByTagName("a");
+  if(msg.clicked == 1){
+    console.log("clicked:",msg.clicked);
     for(var i = 0;i<a.length;i++){
+      color[i] = a[i].getAttribute("color");
       a[i].setAttribute("target","_blank");
-      a[i].style.setProperty("color","cyan")
+      a[i].style.setProperty("color","grey");
+      a[i].className += " newtab";
+    }
+  } else if (msg.clicked == 0) {
+    console.log("clicked:",msg.clicked);
+    for(var i = 0;i<a.length;i++){
+      if(a[i].classList.contains("newtab")){
+        console.log("yes");
+        a[i].classList.remove("newtab");
+        a[i].style.setProperty("color",color[i]);
+        a[i].removeAttribute("target");
+      }
     }
   }
 });
