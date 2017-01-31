3f5ac5b14ac85eaf04784c2fe26c777648fb0b7c
Ritik <saxenar444@gmail.com>
2017-01-31 21:59:08 +0530

    changed icon colors, added change color on click

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
 var clicked = Number(0);
 chrome.browserAction.onClicked.addListener(function(tab){
   clicked = 1-clicked;
-  console.log(clicked+"sd");
+  switch (clicked) {
+    case 1:
+    chrome.browserAction.setIcon({path: 'light.png'});
+      break;
+    case 0:
+    chrome.browserAction.setIcon({path: 'dark.png'});
+      break;
+  }
   chrome.tabs.sendMessage(tab.id,{clicked:clicked});
 });
--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 var color = [];
 chrome.runtime.onMessage.addListener(function(msg,sender){
-  console.log("dsd");
   var a = document.getElementsByTagName("a");
   if(msg.clicked == 1){
     console.log("clicked:",msg.clicked);
     for(var i = 0;i<a.length;i++){
-      color[i] = a[i].getAttribute("color");
       a[i].setAttribute("target","_blank");
-      a[i].style.setProperty("color","grey");
       a[i].className += " newtab";
     }
   } else if (msg.clicked == 0) {
     console.log("clicked:",msg.clicked);
     for(var i = 0;i<a.length;i++){
       if(a[i].classList.contains("newtab")){
-        console.log("yes");
         a[i].classList.remove("newtab");
-        a[i].style.setProperty("color",color[i]);
         a[i].removeAttribute("target");
       }
     }
