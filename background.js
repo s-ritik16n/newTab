434ae358bdaacd91990c699ac5f62cb1cca0a197
Ritik <saxenar444@gmail.com>
2017-02-01 11:49:52 +0530

    added onactivated tabchange listener

--- background.js
+++ background.js
@@ -1,6 +1,21 @@
 var clicked = Number(0);
 chrome.browserAction.onClicked.addListener(function(tab){
   clicked = 1-clicked;
+  changeColor(clicked);
+  chrome.tabs.sendMessage(tab.id,{clicked:clicked});
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo){
+  console.log("tab changed, clicked: ",clicked);
+  if(clicked == 1){
+    chrome.tabs.sendMessage(activeInfo.tabid,{clicked:clicked});
+  }
+  else if(clicked == 0){
+    chrome.tabs.sendMessage(activeInfo.tabid,{clicked:clicked});
+  }
+})
+
+function changeColor(clicked){
   switch (clicked) {
     case 1:
     chrome.browserAction.setIcon({path: 'light.png'});
@@ -9,5 +24,4 @@ chrome.browserAction.onClicked.addListener(function(tab){
     chrome.browserAction.setIcon({path: 'dark.png'});
       break;
   }
-  chrome.tabs.sendMessage(tab.id,{clicked:clicked});
-});
+}
