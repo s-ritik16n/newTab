3d85f69f26c6cf70490f57b23fa9f4a45b939208
Ritik <saxenar444@gmail.com>
2017-02-03 10:43:53 +0530

    added management, onuninstalled, oninstalled, onenbaled, ondisabled

--- background.js
+++ background.js
@@ -1,29 +1,59 @@
-
 chrome.browserAction.onClicked.addListener(function(tab){
   chrome.storage.local.get('click',function(result){
-    if(result.click == 0){
-        chrome.storage.local.set({"click":1},function(){
-        mainProc(tab.id,result.click);
+    console.log("click value(onClicked): "+result.click);
+    if(result.click == "0"){
+        chrome.storage.local.set({"click":"1"},function(){
+          console.log("click value - after click(onClicked): "+result.click);
+          mainProc(tab.id,result.click);
         });
-    } else if (result.click == 1) {
-        chrome.storage.local.set({"click":0},function(){
-        mainProc(tab.id,result.click);
+    } else if (result.click == "1") {
+        chrome.storage.local.set({"click":"0"},function(){
+          console.log("click value - after click(onClicked): "+result.click);
+          mainProc(tab.id,result.click);
         });
     } else if (result.click == undefined) {
-      chrome.storage.local.set({"click":1},function(){
-      mainProc(tab.id,result.click);
+      chrome.storage.local.set({"click":"1"},function(){
+        console.log("click value - after click(onClicked): "+result.click);
+        mainProc(tab.id,result.click);
       });
+    } else if (result.click == null) {
+      chrome.local.get.set({"click":"1"},function(){
+        console.log("click value - after click(onClicked): "+result.click);
+        mainProc(tab.id,result.click);
+      })
     }
   })
 });
 
+chrome.management.onInstalled.addListener(function(extInfo){
+  console.log("installed");
+  chrome.storage.local.clear()
+  //chrome.storage.local.set({"click":"0"});
+})
+
+chrome.management.onEnabled.addListener(function(extInfo){
+  console.log("enabled");
+  chrome.storage.local.clear()
+  //chrome.storage.local.set({"click":"0"});
+})
+
+chrome.management.onUninstalled.addListener(function(extId){
+  console.log("uninstalled");
+  chrome.storage.local.clear();
+})
+
+chrome.management.onDisabled.addListener(function(extInfo){
+  console.log("disabled");
+  chrome.storage.local.clear();
+})
+
 chrome.tabs.onActivated.addListener(function(activeInfo){
   chrome.storage.local.get("click",function(result){
-    console.log("click value: "+result.click);
-    if(result.click == 1){
-      console.log("onActivated");
+    console.log("click value(onActivated): "+result.click);
+    console.log("onActivated");
+    if(result.click == "1"){
       mainProc(activeInfo.tabId,result.click);
-    } else if (result.click == 0) {
+    } else if (result.click == "0") {
       mainProc(activeInfo.tabId,result.click)
     }
   })
@@ -32,8 +62,9 @@ chrome.tabs.onActivated.addListener(function(activeInfo){
 chrome.tabs.onUpdated.addListener(function(tabid,changeInfo,tab) {
   if(changeInfo.status == "complete"){
     chrome.storage.local.get("click",function(result){
-      if(result.click == 1){
-        console.log("onUpdated");
+      console.log("click value(onUpdated): "+result.click);
+      console.log("onUpdated");
+      if(result.click == "1"){
       chrome.tabs.query({active:true,currentWindow:true},function(tabs){
         mainProc(tabid,result.click);
       });
@@ -44,43 +75,28 @@ chrome.tabs.onUpdated.addListener(function(tabid,changeInfo,tab) {
 
 chrome.tabs.onReplaced.addListener(function(addedTabId,removedTabId){
   chrome.storage.local.get("click",function(result){
+    console.log("click value(onReplaced): "+result.click);
     mainProc(addedTabId,result.click);
   })
 })
 
-function changeIcon(clicked){
-  if(clicked == 1){
-    chrome.browserAction.setIcon({path: {"38":"lights.png"}});
-  }else {
-    chrome.browserAction.setIcon({path: {"38":"dark.png"}});
-  }
-}
-
-function activate(id){
-  chrome.storage.local.get("click",function(result){
-    if(result.click == 1){
-      console.log("activate");
-      mainProc(id,result.click);
-    }
-  })
-}
-
 function clickListener(id){
   chrome.storage.local.get("click",function(result){
-    if (result.click == 0) {
-      chrome.storage.local.set({"click":1},function(){
+    console.log("click value(clickListener): "+result.click);
+    if (result.click == "0") {
+      chrome.storage.local.set({"click":"1"},function(){
         mainProc(id,result.click);
       });
-    } else if (result.click == 1) {
-      chrome.storage.local.set({"click":0},function(){
+    } else if (result.click == "1") {
+      chrome.storage.local.set({"click":"0"},function(){
         mainProc(id,result.click);
       });
     } else if (result.click == null) {
-      chrome.storage.local.set({"click":1},function(){
+      chrome.storage.local.set({"click":"1"},function(){
         mainProc(id,result.click);
       });
     } else if (result.click == undefined) {
-      chrome.storage.local.set({"click":1},function(){
+      chrome.storage.local.set({"click":"1"},function(){
         mainProc(id,result.click);
       });
     }
@@ -90,4 +106,12 @@ function clickListener(id){
 function mainProc(id,click){
   changeIcon(click);
   chrome.tabs.sendMessage(id,{clicked:click});
+}
+
+function changeIcon(clicked){
+  if(clicked == 1){
+    chrome.browserAction.setIcon({path: {"38":"lights.png"}});
+  }else {
+    chrome.browserAction.setIcon({path: {"38":"dark.png"}});
+  }
 }
