c76660a50a2e04c68ebe59a7786b613aab3c23fd
Ritik <saxenar444@gmail.com>
2017-02-03 11:28:17 +0530

    handling unexpected

--- background.js
+++ background.js
@@ -9,30 +9,13 @@ chrome.browserAction.onClicked.addListener(function(tab){
         chrome.storage.local.set({"click":0},function(){
           mainProc(tab.id,0);
         });
+    } else {
+      chrome.storage.local.set({"click":1},function(){
+        mainProc(tab.id,1);
+      });
     }
   })
 });
-
-chrome.management.onInstalled.addListener(function(extInfo){
-  console.log("installed");
-  chrome.storage.local.clear()
-})
-
-chrome.management.onEnabled.addListener(function(extInfo){
-  console.log("enabled");
-  chrome.storage.local.clear()
-})
-
-chrome.management.onUninstalled.addListener(function(extId){
-  console.log("uninstalled");
-  chrome.storage.local.clear();
-})
-
-chrome.management.onDisabled.addListener(function(extInfo){
-  console.log("disabled");
-  chrome.storage.local.clear();
-})
-
 chrome.tabs.onActivated.addListener(function(activeInfo){
   chrome.storage.local.get("click",function(result){
     console.log("click value(onActivated): "+result.click);
@@ -62,6 +45,29 @@ chrome.tabs.onReplaced.addListener(function(addedTabId,removedTabId){
     console.log("click value(onReplaced): "+result.click);
     mainProc(addedTabId,result.click);
   })
+})
+
+
+chrome.management.onInstalled.addListener(function(extInfo){
+  console.log("installed");
+  chrome.storage.local.clear()
+})
+
+chrome.management.onEnabled.addListener(function(extInfo){
+  console.log("enabled");
+  chrome.storage.local.clear()
+})
+
+chrome.management.onUninstalled.addListener(function(extId){
+  console.log("uninstalled");
+  chrome.storage.local.remove("click");
+  chrome.storage.local.clear();
+})
+
+chrome.management.onDisabled.addListener(function(extInfo){
+  console.log("disabled");
+  chrome.storage.local.remove("click");
+  chrome.storage.local.clear();
 })
 
 function mainProc(id,click){
