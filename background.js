3180856e2ff91b84b6efb9dc8b2e1b2a875d31a6
Ritik <saxenar444@gmail.com>
2017-02-03 11:08:01 +0530

    debugging successful, no bugs for now

--- background.js
+++ background.js
@@ -1,27 +1,27 @@
 chrome.browserAction.onClicked.addListener(function(tab){
   chrome.storage.local.get('click',function(result){
     console.log("click value(onClicked): "+result.click);
-    if(result.click == "0"){
-        chrome.storage.local.set({"click":"1"},function(){
-          console.log("click value - after click(onClicked): "+result.click);
-          mainProc(tab.id,result.click);
+    if(result.click == 0){
+        chrome.storage.local.set({"click":1},function(){
+          //console.log("click value - after click(onClicked): "+result.click);
+          mainProc(tab.id,1);
         });
-    } else if (result.click == "1") {
-        chrome.storage.local.set({"click":"0"},function(){
-          console.log("click value - after click(onClicked): "+result.click);
-          mainProc(tab.id,result.click);
+    } else if (result.click == 1) {
+        chrome.storage.local.set({"click":0},function(){
+          //console.log("click value - after click(onClicked): "+result.click);
+          mainProc(tab.id,0);
         });
-    } else if (result.click == undefined) {
-      chrome.storage.local.set({"click":"1"},function(){
-        console.log("click value - after click(onClicked): "+result.click);
+    } /*else if (result.click == undefined) {
+      chrome.storage.local.set({"click":1},function(){
+        //console.log("click value - after click(onClicked): "+result.click);
         mainProc(tab.id,result.click);
       });
     } else if (result.click == null) {
-      chrome.local.get.set({"click":"1"},function(){
-        console.log("click value - after click(onClicked): "+result.click);
+      chrome.local.get.set({"click":1},function(){
+        //console.log("click value - after click(onClicked): "+result.click);
         mainProc(tab.id,result.click);
       })
-    }
+    }*/
   })
 });
 
@@ -50,10 +50,10 @@ chrome.management.onDisabled.addListener(function(extInfo){
 chrome.tabs.onActivated.addListener(function(activeInfo){
   chrome.storage.local.get("click",function(result){
     console.log("click value(onActivated): "+result.click);
-    console.log("onActivated");
-    if(result.click == "1"){
+    //console.log("onActivated");
+    if(result.click == 1){
       mainProc(activeInfo.tabId,result.click);
-    } else if (result.click == "0") {
+    } else if (result.click == 0) {
       mainProc(activeInfo.tabId,result.click)
     }
   })
@@ -63,12 +63,14 @@ chrome.tabs.onUpdated.addListener(function(tabid,changeInfo,tab) {
   if(changeInfo.status == "complete"){
     chrome.storage.local.get("click",function(result){
       console.log("click value(onUpdated): "+result.click);
-      console.log("onUpdated");
-      if(result.click == "1"){
-      chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+      //console.log("onUpdated");
+      if(result.click == 1){
         mainProc(tabid,result.click);
-      });
-      }
+      //chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+      //});
+    } else if (result.click == 0) {
+      mainProc(tabid,result.click);
+    }
     })
   }
 })
@@ -80,38 +82,15 @@ chrome.tabs.onReplaced.addListener(function(addedTabId,removedTabId){
   })
 })
 
-function clickListener(id){
-  chrome.storage.local.get("click",function(result){
-    console.log("click value(clickListener): "+result.click);
-    if (result.click == "0") {
-      chrome.storage.local.set({"click":"1"},function(){
-        mainProc(id,result.click);
-      });
-    } else if (result.click == "1") {
-      chrome.storage.local.set({"click":"0"},function(){
-        mainProc(id,result.click);
-      });
-    } else if (result.click == null) {
-      chrome.storage.local.set({"click":"1"},function(){
-        mainProc(id,result.click);
-      });
-    } else if (result.click == undefined) {
-      chrome.storage.local.set({"click":"1"},function(){
-        mainProc(id,result.click);
-      });
-    }
-  })
-}
-
 function mainProc(id,click){
   changeIcon(click);
   chrome.tabs.sendMessage(id,{clicked:click});
 }
 
-function changeIcon(clicked){
-  if(clicked == 1){
+function changeIcon(click){
+  if(click == 1){
     chrome.browserAction.setIcon({path: {"38":"lights.png"}});
-  }else {
+  }else if (click == 0) {
     chrome.browserAction.setIcon({path: {"38":"dark.png"}});
   }
 }
